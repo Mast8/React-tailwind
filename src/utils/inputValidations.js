c85bb2f4abca598328a6134a0b1011c8f1df325b
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-05-04 10:37:13 -0400

    react form tailwind

---
+++ src/utils/inputValidations.js
@@ -0,0 +1,98 @@
+/*-------------------------------------------------------------------
+|  🐼 Input Validators 
+|
+|  🐯 Purpose: THIS FILE CONTAINS ALL THE VALIDATORS OBJECTS
+|
+|  🐸 Returns:  -
+*-------------------------------------------------------------------*/
+
+export const name_validation = {
+  name: 'name',
+  label: 'name',
+  type: 'text',
+  multiType : false,
+  id: 'name',
+  placeholder: 'Name ...',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    maxLength: {
+      value: 30,
+      message: '30 characters max',
+    },
+  },
+}
+
+export const desc_validation = {
+  name: 'description',
+  label: 'description',
+  multiline: true,
+  id: 'description',
+  placeholder: 'Description ...',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    maxLength: {
+      value: 200,
+      message: '200 characters max',
+    },
+  },
+}
+
+export const password_validation = {
+  name: 'password',
+  label: 'password',
+  type: 'password',
+  multiType : true,
+  id: 'password',
+  placeholder: 'type password ...',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    minLength: {
+      value: 6,
+      message: 'min 6 characters',
+    },
+  },
+}
+
+export const num_validation = {
+  name: 'num',
+  label: 'number',
+  type: 'number',
+  multiType : false,
+  id: 'num',
+  placeholder: 'write a number',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+  },
+}
+
+export const email_validation = {
+  name: 'email',
+  label: 'email address',
+  type: 'email',
+  multiType : false,
+  id: 'email',
+  placeholder: 'Email address',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    pattern: {
+      value:
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: 'Invalid',
+    },
+  },
+}
